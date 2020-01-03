3e4794cc78a8cfdc0222e3c53652492e73cdef8e
Marcus Hultman <marcus.hultman1@gmail.com>
2020-01-03 18:02:49 +0100

    deploy

---
+++ precache-manifest.f309ee31e4adfd1c8b87ae7821b28ff9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b841f50d329217ac2c25",
+    "url": "/achtung/css/app.a72220b9.css"
+  },
+  {
+    "revision": "b841f50d329217ac2c25",
+    "url": "/achtung/js/app.b8381f90.js"
+  },
+  {
+    "revision": "8a25f1a38c1093a3fbcb",
+    "url": "/achtung/js/chunk-vendors.994b26e1.js"
+  },
+  {
+    "revision": "93dc7020a9ba9117753da197e39c7a6c",
+    "url": "/achtung/index.html"
+  },
+  {
+    "revision": "095d248b50e2d06ee5d58abd1145858b",
+    "url": "/achtung/404.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/achtung/robots.txt"
+  }
+];
